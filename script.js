457ad943e9c75691f04b5e539e28dd9a546bb375
mehaned <mehaned.ahmed7@yahoo.com>
2023-01-19 10:15:05 +0100

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-const express = require('express');
-const app = express();
-const server = http.createServer(app);
+$(document).ready(function(){
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
 
-app.use(express.static("public"));
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
 
-app.get('/',  (req, res) => {
-  res.render('index.html')
-})
-const PORT = process.env.PORT || 3000; 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+  });
+
+  // smooth scrolling 
+  $('a[href*="#"]').on('click',function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop : $($(this).attr('href')).offset().top,
+    },
+      500, 
+      'linear'
+    );
+  });
+
+});
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".my-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+    // Skills Animate Width
+    if (window.scrollY >= section.offsetTop - 250) {
+      progressSpans.forEach((span) => {
+        span.style.width = span.dataset.width;
+      });
+    }
+
+}
+
+
